77b03e7ad1efca37836594d9f50e722073b4be0d
Naif Alshaia <n.alshaia@tamkeentech.sa>
2018-07-30 14:27:13 +0300

    init structure

--- src/AddressController.php
+++ src/AddressController.php
@@ -3,10 +3,32 @@
 namespace Naif\Saudiaddress;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class AddressController extends Controller
 {
-    public function GeoCode($lat, $lng){
-        echo $lat;
+    public function geoCode($lat, $lng){
+        return [$lat,$lng];
     }
+
+    public function verify(Request $request)
+    {
+        return $request->all();
+    }
+
+    public function regions()
+    {
+        return 'regions';
+    }
+
+    public function cities($region_id)
+    {
+        return $region_id;
+    }
+
+    public function districts($city_id)
+    {
+        return $city_id;
+    }
+
 }
--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,10 @@
 <?php
 
-Route::get('saudiaddress', function(){
-    echo 'Salam Saudi Arabia';
-});
-
-Route::get('saudiaddress/geocode/{lat}/{lng}', 'Naif\Saudiaddress\AddressController@GeoCode');
+Route::get('saudiaddress/geocode/{lat}/{lng}', 'Naif\Saudiaddress\AddressController@geoCode');
+Route::post('saudiaddress/verify', 'Naif\Saudiaddress\AddressController@verify');
+Route::get('saudiaddress/regions', 'Naif\Saudiaddress\AddressController@regions');
+Route::get('saudiaddress/cities/{region_id}', 'Naif\Saudiaddress\AddressController@cities');
+Route::get('saudiaddress/districts/{city_id}', 'Naif\Saudiaddress\AddressController@districts');
 
 
 ?>
